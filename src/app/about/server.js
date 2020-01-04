189d38f01e37a3d57165ecb566d80269ecf278be
Awanish kumar <awanishkr91@gmail.com>
2020-01-05 00:28:58 +0530

    server.js file gives port name

--- src/app/about/server.js
+++ src/app/about/server.js
@@ -1,16 +1,15 @@
 //install express server
 const express = require('express');
-const path = require('path');
 const app = express();
 
 
 // serve only static file from the dist directory
 //replace the 'dist/<to_your_project_name>'
-app.use(express.static(__dirname + '/dist/mi-profile'));
+app.use(express.static(__dirname + '/dist'));
 
 app.get('*',function(req,res){
 //replace the 'dist/<to_your_project_name>/index.html' 
 res.sendFile(path.join(__dirname + '/dist/mi-profile/index.html'));
 });
 // start the app by listening on the default heroku port
-app.listen(process.env.port)
+app.listen(process.env.port || 8080)

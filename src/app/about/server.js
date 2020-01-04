7b75de31071fa237f99ba91cb9b1eb28ecdaf6e0
Awanish kumar <awanishkr91@gmail.com>
2020-01-05 00:55:15 +0530

    small changes into server.js file

--- src/app/about/server.js
+++ src/app/about/server.js
@@ -1,10 +1,17 @@
 //install express server
 const express = require('express');
 const app = express();
+const path = require('path');
 
 
 // serve only static file from the dist directory
 //replace the 'dist/<to_your_project_name>'
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/mi-profile'));
+
+
+app.get('/*',function(req,res){
+//replace the 'dist/<to_your_project_name>/index.html' 
+res.sendFile(path.join(__dirname + '/dist/mi-profile/index.html'));
+});
 // start the app by listening on the default heroku port
 app.listen(process.env.port || 8080)

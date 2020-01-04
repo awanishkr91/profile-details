68ae6b2c409af0efa21a0988a58cb9a0db093b3d
Awanish kumar <awanishkr91@gmail.com>
2020-01-05 00:36:18 +0530

    server.js file having some changes

--- src/app/about/server.js
+++ src/app/about/server.js
@@ -6,10 +6,5 @@ const app = express();
 // serve only static file from the dist directory
 //replace the 'dist/<to_your_project_name>'
 app.use(express.static(__dirname + '/dist'));
-
-app.get('*',function(req,res){
-//replace the 'dist/<to_your_project_name>/index.html' 
-res.sendFile(path.join(__dirname + '/dist/mi-profile/index.html'));
-});
 // start the app by listening on the default heroku port
 app.listen(process.env.port || 8080)
